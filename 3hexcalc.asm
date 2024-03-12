6f5e0f4b276c371b678c5b6b665f50dfd64dc736
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-12 21:18:52 +0700

    Исправлены ошибки

--- 3hexcalc.asm
+++ 3hexcalc.asm
@@ -1,116 +1,106 @@
+.include "library.asm"
+
 .text
-.macro syscall %n
-  li a7, %n
-  ecall
-.end_macro
+main:    #entry point 
 
-.macro exit %ecode
-  li a0, %ecode
-  syscall 93
-.end_macro
+  call readhex
+  beqz s7, skiperror1
+  error "Maximum number of characters exceeded!"
+  
+skiperror1:
+  mv a4, a5
+  li a5, 0
+  
+  call readhex
+  beqz s7, skiperror2
+  error "Maximum number of characters exceeded!"
 
-.macro readch # result in a0[0:7]
-  syscall 12
-.end_macro
+skiperror2:
+  readch 
+  
+  li t2,  0x2B #знак плюс
+  beq a0, t2, addition
+  li t2, 0x2D #знак минус
+  beq a0, t2, subtraction
+  li t2, 0x26 # знак &
+  beq a0, t2, andoperation
+  li t2, 0x7C # знак |
+  beq a0, t2, oroperation
+  
+addition:
+  add s2, a4, a5
+  call printhex
+  j end
+  
+subtraction:
+  sub s2, a4, a5
+  call printhex
+  j end
+  
+andoperation:
+  and s2, a4, a5
+  call printhex
+  j end
+  
+oroperation:
+  or s2, a4, a5
+  call printhex
+  j end
 
-.macro printch # argument in a0[0:7]
-  syscall 11
-.end_macro
+end:
+  exit 0
 
-.macro printhex # вывод шестнадцатиричного числа
-  li a1, 7
-  li s3, 10
-  mv s4, s2
+readhex:
+  li t3, 10
+  li t2, 0 # счетчик цикла
+  li t1, 8
+  li s7, 0 # код ошибки
+  li a0, 0
+  
+readloop:
+  readch
+  beq a0, t3, quitloop # Выход из цикла
+  
+  addi a0, a0, -48
+  blt a0, t3, nothex
+  andi a0, a0, 0xFFFFFFDF # Преобразование к верхнему регистру
+  addi a0, a0, -7 # если число от A - F
+  
+nothex:
+  slli a5, a5, 4
+  add a5, a5, a0
+  addi t2, t2, 1
+  j readloop
+  
+  quitloop:
+  slt s7, t1, t2 # записываем код ошибки переполнения числа
+  ret
+  
+printhex: # вывод шестнадцатиричного числа
+  li t0, 7
+  li t3, 10
   li a0, 10
   printch
   li a0, '0'
   printch
   li a0, 'x'
   printch
+  
 loop:
-  mv t0, a1
+  mv t1, t0
+  mv t4, s2
   
-  mv s4, s2
-  slli t0, t0, 2
-  srl s4, s4, t0
+  slli t1, t1, 2
+  srl t4, t4, t1
   
-  andi a2, s4, 0x0000000f # маска младший neeble в слове
-  blt a2, s3, spot
+  andi a2, t4, 0x0000000f # маска младший neeble в слове
+  blt a2, t3, spot
   addi a2, a2, 7
   
 spot:
   addi a0, a2, 48
   printch
-  addi a1, a1, -1
-  bge a1, x0, loop
-.end_macro
+  addi t0, t0, -1
+  bgez t0, loop
 
-main:    #entry point
-  li a3, 10
-  
-scanfirstnumber:
-  li a0, 0
-  readch
-  beq a0, a3, scansecondnumber # Переход к считыванию следующего числа
-  
-  addi a0, a0, -48
-  blt a0, a3, nothex
-  andi a0, a0, 0xFFFFFFDF # Преобразование к верхнему регистру
-  addi a0, a0, -7 # если число от A - F
-  
-nothex:
-  slli a4, a4, 4
-  add a4, a4, a0
-  j scanfirstnumber
-
-scansecondnumber:
-  li a0, 0
-  readch
-  beq a0, a3, scanoperation # Переход к считыванию операции
-  
-  addi a0, a0, -48
-  blt a0, a3, nothex2
-  andi a0, a0, 0xFFFFFFDF # Преобразование к верхнему регистру
-  addi a0, a0, -7 # если число от A - F
-  
-nothex2:
-  slli a5, a5, 4
-  add a5, a5, a0
-  j scansecondnumber
-
-scanoperation:
-  readch
-  
-  li s1,  0x2B #знак плюс
-  beq a0, s1, addition
-  li s1, 0x2D #знак минус
-  beq a0, s1, subtraction
-  li s1, 0x26 # знак &
-  beq a0, s1, andoperation
-  li s1, 0x7C # знак |
-  beq a0, s1, oroperation
-
-  j scanoperation
-addition:
-  add s2, a4, a5
-  printhex
-  j end
-  
-subtraction:
-  sub s2, a4, a5
-  printhex
-  j end
-  
-andoperation:
-  and s2, a4, a5
-  printhex
-  j end
-  
-oroperation:
-  or s2, a4, a5
-  printhex
-  j end
-
-end:
-  exit 0
-  
+  ret
