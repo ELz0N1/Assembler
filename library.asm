669fc2230722255386fb384e9423bb29b5690247
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-12 21:19:26 +0700

    Add files via upload

---
+++ library.asm
@@ -0,0 +1,54 @@
+.macro syscall %n
+  li a7, %n
+  ecall
+.end_macro
+
+.macro exit %ecode
+  li a0, %ecode
+  syscall 93
+.end_macro
+
+.macro error %str
+.data
+str: .asciz %str
+.text
+  la a0, str
+  syscall 4
+  exit 1
+.end_macro
+
+.macro push %r
+  addi sp, sp, -4
+  sw %r, 0(sp)
+.end_macro
+
+.macro pop %r
+  lw %r, 0(sp)
+  addi sp, sp, 4
+.end_macro
+
+.macro push2 %r1, %r2
+  addi sp, sp, -8
+  sw %r1, 0(sp)
+  sw %r2, 4(sp)
+.end_macro
+
+.macro pop2 %rd1, %rd2
+  lw %r1, 0(sp)
+  lw %r2, 4(sp)
+  addi sp, sp, 8
+.end_macro
+
+.macro swap %r1, %r2
+  xor %r1, %r1, %r2
+  xor %r2, %r2, %r1
+  xor %r1, %r1, %r2
+.end_macro
+
+.macro readch # result in a0[0:7]
+  syscall 12
+.end_macro
+
+.macro printch # argument in a0[0:7]
+  syscall 11
+.end_macro
