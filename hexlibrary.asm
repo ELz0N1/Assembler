dd415db64726f69b1cdcdb534222543a81cf1dce
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-16 16:44:39 +0700

    исправлены новые замечания

--- hexlibrary.asm
+++ hexlibrary.asm
@@ -23,58 +23,51 @@ symbolerror:
   error "Inncorect symbol!"
 
 
-readhex:
+readhex: # int readhex()
   push3 ra, s0, s1
-  li t4, 10
-  li t2, 0 # счетчик цикла
+  li s1, 0 # счетчик цикла
   li s0, 0
-  li s1, 0
   
 readloop:
   readch
-
+  
+  li t4, 10
   beq a0, t4, quitloop # Выход из цикла
   
   call symbolcheck
 
   slli s0, s0, 4
   add s0, s0, a0
-  addi t2, t2, 1
+  addi s1, s1, 1
   j readloop
   
 quitloop:
   li t1, 8 # количество допустимых разрядов числа
-  slt s1, t1, t2 # записываем код ошибки переполнения числа
-  bnez s1, lengtherror
+  slt t2, t1, s1 # записываем код ошибки переполнения числа
+  bnez t2, lengtherror
   mv a0, s0
   pop3 ra, s0, s1
-  ret 
+  ret  
 lengtherror:
   error "Maximum number of characters exceeded!" 
   
   
-printhex: # вывод шестнадцатиричного числа
-  push3 ra, s1, s2
-  li s1, 0
-  li s2, 0
-  mv s1, a0
-  mv s2, s1
+printhex: # void printhex(n)
   li t0, 7
   li t3, 10
-  li a0, 10
-  printch
+  mv t4, a0 #s1
+  mv t5, t4 #s2
+
   li a0, '0'
   printch
   li a0, 'x'
   printch
   
 loop:
-  mv t1, t0
-  mv s1, s2
-  slli t1, t1, 2
-  srl s1, s1, t1
-  
-  andi t2, s1, 0x0000000f # маска младший neeble в слове
+  slli t1, t0, 2
+  srl t4, t5, t1
+
+  andi t2, t4, 0x0000000f # маска младший neeble в слове
   blt t2, t3, spot
   addi t2, t2, 7
   
@@ -84,5 +77,4 @@ spot:
   addi t0, t0, -1
   bgez t0, loop
   
-  pop3 ra, s1, s2
   ret
