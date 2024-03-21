9aeb03d89547dfee49b59be4b4c9128eca43d078
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-21 23:06:13 +0700

    Update divisionlibrary.asm

--- divisionlibrary.asm
+++ divisionlibrary.asm
@@ -1,29 +1,30 @@
 div10:
-  mv a2, s0
-  srli t1, a2, 1
-  srli a2, a2, 2
-  li t4, 0 # счетчик
-divloop:
-  li t3, 10
-  blt t1, t3, endloop
-  sub t1, t1, t3
-  addi t4, t4, 1
-  j divloop
-endloop:
-  sub t2, a2, t4
-  srli a1, t2, 1
-  
-  mv a0, a1
-  ret
-  
-mod10:
-  push4 ra, s0, s1, s2
-  mv s2, s0
-  call div10
+  push3 ra, s0, s1
+  mv s1, a0
+  li s0, 0
+  li t0, 10
+  blt a0, t0, enddiv10
   mv s0, a0
-  li s1, 10
+  srli a0, a0, 1
+  call div10
+  srli s0, s0, 2
+  sub s0, s0, a0
+  srli s0, s0, 1
+enddiv10:
+  mv a0, s0
+  li a1, 10
   call multiply
-  sub a1, s2, a0
-  mv a0, a1
-  pop4 ra, s0, s1, s2
+  slt a0, s1, a0
+  sub a0, s0, a0
+  pop3 ra, s0, s1
+  ret
+ 
+mod10:
+  push2 ra, s0
+  mv s0, a0
+  call div10
+  li a1, 10
+  call multiply
+  sub a0, s0, a0
+  pop2 ra, s0
   ret
