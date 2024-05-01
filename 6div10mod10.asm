e486c1fdaa139699008b8191355d429d595bdc3e
Danil Zhukov <zhukov.danila2004@yandex.ru>
2024-05-02 01:34:34 +0700

    added test for mul10

--- 6div10mod10.asm
+++ 6div10mod10.asm
@@ -8,7 +8,6 @@
 main:
   call readhex
   mv s0, a0
-  
   call div10
   call printhex
   
@@ -17,5 +16,12 @@ main:
   mv a0, s0
   call mod10
   call printhex
+  
+  newline
+  
+  mv a0, s0
+  call mul10
+  call printhex
+  
   
   exit 0
